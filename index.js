89fc75f3b6d540c4277a05a1557e96910b670c34
Christopher Robinson <chrispaul_robinson@hotmail.com>
2021-03-21 07:20:14 -0500

    Add other commands

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv");
 const Discord = require("discord.js");
 
+const { prefix } = require("./config.json");
+
 //Loads data from .env files into process.env variable
 dotenv.config();
 
@@ -14,9 +16,63 @@ client.once("ready", () => {
 });
 
 client.on("message", (message) => {
-  console.log(message.content);
-  if (message.content === "~>Suck") {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "suck") {
     message.channel.send("Yuh mada");
+  } else if (command === "args-info") {
+    if (!args.length) {
+      return message.channel.send(
+        `You didn't provide any arguments, ${message.author}!`
+      );
+    }
+
+    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+  } else if (command === "kick") {
+    if (!message.mentions.users.size) {
+      return message.reply("you need to tag a user in order to kick them!");
+    }
+
+    // grab the "first" mentioned user from the message
+    // this will return a `User` object, just like `message.author`
+    const taggedUser = message.mentions.users.first();
+
+    message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  } else if (command === "avatar") {
+    if (!message.mentions.users.size) {
+      return message.channel.send(
+        `Your avatar: <${message.author.displayAvatarURL({
+          format: "png",
+          dynamic: true,
+        })}>`
+      );
+    }
+
+    const avatarList = message.mentions.users.map((user) => {
+      return `${user.username}'s avatar: <${user.displayAvatarURL({
+        format: "png",
+        dynamic: true,
+      })}>`;
+    });
+
+    // send the entire array of strings as a message
+    // by default, discord.js will `.join()` the array with `\n`
+    message.channel.send(avatarList);
+  } else if (command === "prune") {
+    const amount = parseInt(args[0]) + 1;
+
+    if (isNaN(amount)) {
+      return message.reply("that doesn't seem to be a valid number.");
+    } else if (amount < 2 || amount > 100) {
+      return message.reply("you need to input a number between 1 and 99.");
+    }
+
+    message.channel.bulkDelete(amount);
+
+    // ...
   }
 });
 
